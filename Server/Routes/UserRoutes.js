62c128017d9738425bd47aa63a4575a41a9e6d40
yeasintamim <yeasin47@student.sust.edu>
2022-08-31 22:18:14 +0600

    update user profile api

--- Server/Routes/UserRoutes.js
+++ Server/Routes/UserRoutes.js
@@ -88,5 +88,37 @@ userRouter.get(
 ))
 
 
+//UPDATE PROFILE
+userRouter.put(
+    "/profile",
+    protect,
+    asyncHandler(async(req,res) =>{
+        const user = await User.findById(req.user._id);
+        if (user) {
+            user.name = req.body.name || user.name
+            user.email = req.body.email || user.email
+            if(req.body.password){
+                user.password = req.body.password
+            }
+            const updatedUser = await user.save();
+            res.json({
+                _id:updatedUser._id,
+                name:updatedUser.name,
+                email:updatedUser.email,
+                isAdmin:updatedUser.isAdmin,
+                token:generateToken(updatedUser._id),
+                createdAt:updatedUser.createdAt
+            });
+        } else {
+            res.status(404);
+            throw new Error("User not found");
+        }
+
+    }
+))
+
+
+
+
 
 export default userRouter
