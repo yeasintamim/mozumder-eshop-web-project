a5297e88dc2039e0b475be3b31ef5b042fd2be82
yeasintamim <yeasin47@student.sust.edu>
2022-08-31 14:39:29 +0600

    login screen issues solved

--- frontend/src/Redux/Reducers/userReducers.js
+++ frontend/src/Redux/Reducers/userReducers.js
@@ -9,9 +9,9 @@ import {
 export const userLoginReducer = (state = {},action ) =>{
     switch(action.type){
         case USER_LOGIN_REQUEST:
-            return { ...state,loading: true,products:[]};
+            return { loading: true};
         case USER_LOGIN_SUCCESS:
-            return {loading: false,product:action.payload};
+            return {loading: false,userInfo:action.payload};
         case USER_LOGIN_FAIL:
             return {loading: false,error:action.payload};
         case USER_LOGOUT:
--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -3,20 +3,28 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension"
 import { productDetailsReducer, productListReducer } from "./Reducers/ProductReducers";
 import { cartReducer } from "./Reducers/CartReducers";
-
-
+import { userLoginReducer } from "./Reducers/userReducers";
 
 const reducer = combineReducers({
     productList : productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer, 
+    userLogin: userLoginReducer, 
+
 });
 
 const cartItemsFromLocalStorage = localStorage.getItem("cartItems")?JSON.parse(localStorage.getItem("cartItems")):[]
+
+//login
+const userInfoFromLocalStorage = localStorage.getItem("userInfo")?JSON.parse(localStorage.getItem("userInfo")):null
+
+
 const initialState = {
     cart:{
         cartItems:cartItemsFromLocalStorage
     }
+    ,
+    userLogin:{userInfo:userInfoFromLocalStorage}
 };  
 const middleware = [ thunk ];
 
